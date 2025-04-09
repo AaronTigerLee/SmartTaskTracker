21f92b2526e68db1d31aa0422d98ef1cdf66ea58
AaronTigerLee <aaron.t.lee1@outlook.com>
2025-04-09 09:41:28 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartTaskTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartTaskTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
